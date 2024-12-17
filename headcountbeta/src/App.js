15da3ee51e671e28688e8437c25dcb604f612d29
yasithrashan2003 <y.rashan22@gmail.com>
2024-12-17 22:39:04 +0530

    feat: Add Real Time Head count feature

---
+++ headcountbeta/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { HeadCount } from './components/HeadCount';
+
+function App() {
+  return (
+    <div className="App">
+      <HeadCount/>
+    </div>
+  );
+}
+
+export default App;
---
+++ headcountbeta/src/components/HeadCount.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export const HeadCount = () => {
+  return (
+    <div>Hello world</div>
+  )
+}
