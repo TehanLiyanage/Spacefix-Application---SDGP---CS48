adf5293783cd9145fef32f300ba72eb9289015c4
Samee205 <sameeha.20221996@iit.ac.lk>
2025-03-21 10:23:07 +0530

    feat:(Labkeeper Dashboard)Sidebar.jsx

--- src/components/dashboard/iit/labkeeperDashboardcomponents/Sidebar.jsx
+++ src/components/dashboard/iit/labkeeperDashboardcomponents/Sidebar.jsx
@@ -22,14 +22,14 @@ const Sidebar = ({ activePage, setActivePage, isOpen, toggleSidebar, isMobile })
         <div className="flex items-center justify-between p-4 border-b border-cyan-400">
           {isMobile ? (
             <>
-              <h1 className="text-xl text-white">LABKEEP PORTAL</h1>
+              <h1 className="text-xl text-white">LABKEEPER PORTAL</h1>
               <button onClick={toggleSidebar} className="text-gray-300 hover:text-white">
                 <X className="w-5 h-5" />
               </button>
             </>
           ) : isOpen ? (
             <>
-              <h1 className="text-xl text-white">LABKEEP PORTAL</h1>
+              <h1 className="text-xl text-white">LABKEEPER PORTAL</h1>
               <button onClick={toggleSidebar} className="text-gray-300 hover:text-white">
                 <ChevronLeft className="w-5 h-5" />
               </button>
